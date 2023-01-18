c586f4e3d0361c63b94979b71e195c1ae83706c0
D00M3RB0Y <122471874+D00M3RB0Y@users.noreply.github.com>
2023-01-18 13:31:55 -0600

    Agregamos lista de canciones

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const songs = [
+    {
+        name: 'Torpedo',
+        path: 'music/Song 1.mp3',
+        artist: 'Rae Sremmurd',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'Look Alive',
+        path: 'music/Song 2.mp3',
+        artist: 'Rae Sremmurd',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'Glock In My Benz',
+        path: 'music/Song 3.mp3',
+        artist: 'Smokepurp',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'Just Wanna Rock',
+        path: 'music/Song 4.mp3',
+        artist: 'Lil Uzi Vert',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'Aim For The Moon',
+        path: 'music/Song 5.mp3',
+        artist: 'Pop Smoke ft Quavo',
+        cover: 'images/cover 5.png'
+    }
+]
